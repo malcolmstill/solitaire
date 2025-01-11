d0ee12c8b5873435d785961e7c07fcec40b9e52b
Malcolm Still <malcolm.still@gmail.com>
2025-01-11 14:00:56 +0000

    Capture windowing API annoyances and move on

---
+++ TODO.md
@@ -0,0 +1,6 @@
+- Instead of storing full copies of board, mutate the board but store the move
+  - Store less data, but still able to undo
+    - Undo either full rerun of changes
+    - Or likely easier to do animation with: each action has the reverse action
+  - Stupid idea (maybe): double entry bookkeeping for moves
+    - [take waste, put spades]
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const r = @cImport(@cInclude("raylib.h"));
+const std = @import("std");
 
 const CardVisual = @import("card.zig").CardVisual;
 
@@ -7,6 +8,32 @@ pub fn main() !void {
     r.SetTargetFPS(60);
     defer r.CloseWindow();
 
+    // Ideally we'd turn this on:
+    //
+    // r.EnableEventWaiting();
+    //
+    // This basically goes 100% to sleep until there is some event.
+    //
+    // Unfortunately, as it stands with the raylib library this does
+    // not count a vsync as an event, nor does it, I believe, allow
+    // for creating some timer that also counts as an event.
+    //
+    // Moreover, raylib only seems to allow querying what the current state
+    // of inputs is, not really getting those events in some sort of queue.
+    // Whilst I think it is possible to build that queue by doing a state
+    // query and looking for changes to turn into events, the library does
+    // not directly support this.
+    //
+    // I'm going to make an assertion: the most flexible API for a graphics
+    // windowing library is one that provides an iterator to pull events
+    // out of a queue. This puts all of the control into the consumer of the
+    // library.
+    //
+    // Second assertion: it's likely possible on all platforms to get vsync
+    // information and so that should be available as an event.
+    //
+    // So having written all this, we're just going to go with the busy
+    // loop, because we're not trying to solve window libraries here.
     while (!r.WindowShouldClose()) {
         r.BeginDrawing();
         defer r.EndDrawing();
