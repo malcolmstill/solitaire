d4da7703d009560fe78f60f9978e2ea90f7f4d75
Malcolm Still <malcolm.still@gmail.com>
2025-12-22 02:32:24 +0000

    fix

---
+++ src/geom.zig
@@ -0,0 +1,14 @@
+const Point = @import("point.zig").Point;
+
+pub const STOCK_LOCUS: Point = .{ .x = 20, .y = 20 };
+
+pub const CARD_WIDTH = 60.0;
+pub const CARD_HEIGHT = CARD_WIDTH * CARD_RATIO;
+pub const CARD_STROKE = 1.0;
+
+pub const CARD_RATIO = 1.4;
+pub const CARD_BACK_GUTTER = 6.0;
+pub const CARD_BACK_WIDTH = CARD_WIDTH - 2 * CARD_BACK_GUTTER;
+pub const CARD_BACK_HEIGHT = CARD_HEIGHT - 2 * CARD_BACK_GUTTER;
+pub const CARD_STROKE_WIDTH = CARD_WIDTH + 2.0 * CARD_STROKE;
+pub const CARD_STROKE_HEIGHT = CARD_HEIGHT + 2.0 * CARD_STROKE;
