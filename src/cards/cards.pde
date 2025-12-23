a62f6cb10f077952de83bb48e440d10c76878391
Malcolm Still <malcolm.still@gmail.com>
2025-12-23 12:24:50 +0000

    generate artwork offline

---
+++ src/cards/cards.pde
@@ -0,0 +1,153 @@
+PFont gbs;
+PFont gbl;
+PGraphics pg;
+
+int WIDTH = 61;
+int HEIGHT = 85;
+int STROKE_WIDTH = 1;
+int PADDING = 7;
+int WIDTH_WITH_STROKE = WIDTH + 1;
+int HEIGHT_WITH_STROKE = HEIGHT + 1;
+
+int BACK_PADDING = 8;
+
+void setup() {
+  pixelDensity(1);
+  size(806, 430, P2D);
+  
+  pg = createGraphics(806, 430, JAVA2D);
+
+  printArray(PFont.list());
+  gbs = createFont("Georgia-Bold", 12);
+  gbl = createFont("Georgia-Bold", 48);
+} 
+
+String character(int c) {
+  if (c == 0) {
+    return "A"; 
+  } else if (c == 1) {
+    return "2"; 
+  } else if (c == 2) {
+    return "3"; 
+  } else if (c == 3) {
+    return "4"; 
+  } else if (c == 4) {
+    return "5"; 
+  } else if (c == 5) {
+    return "6"; 
+  } else if (c == 6) {
+    return "7"; 
+  } else if (c == 7) {
+    return "8"; 
+  } else if (c == 8) {
+    return "9"; 
+  } else if (c == 9) {
+    return "10"; 
+  } else if (c == 10) {
+    return "J"; 
+  } else if (c == 11) {
+    return "Q"; 
+  } else if (c == 12) {
+    return "K";
+  } else {
+    throw new RuntimeException("Bad rank");
+  }
+}
+
+String suitCharacter(int suit) {
+  if (suit == 0) {
+    return "♠"; 
+  } else if (suit == 1) {
+    return "♥"; 
+  } else if (suit == 2) {
+    return "♦"; 
+  } else if (suit == 3) {
+    return "♣"; 
+  } else {
+    throw new RuntimeException("Bad suit");
+  }
+}
+
+void setSuitColor(int suit) {
+  if (suit == 0) {
+    pg.fill(0, 0, 0);
+  } else if (suit == 1) {
+    pg.fill(235, 0, 0);
+  } else if (suit == 2) {
+    pg.fill(235, 0, 0);
+  } else if (suit == 3) {
+    pg.fill(0, 0, 0);
+  } else {
+    throw new RuntimeException("Bad suit");
+  }
+}
+
+void draw() {
+  pg.beginDraw();
+  pg.background(0);
+  pg.clear();
+  
+  float sw = 1;
+  pg.strokeWeight(sw);
+
+  for (int suit = 0; suit < 4; suit ++) {
+    String suit_char = suitCharacter(suit);
+    
+    for (int c = 0; c < 13; c++) {
+      pg.pushMatrix();
+      pg.translate(c*WIDTH_WITH_STROKE, suit*HEIGHT_WITH_STROKE);
+  
+      pg.fill(255);
+      pg.stroke(40);
+      pg.rect(0, 0, WIDTH, HEIGHT, 8);
+      
+      setSuitColor(suit);
+     
+      pg.textFont(gbs);
+      pg.textAlign(CENTER, CENTER);
+      
+      for (int i = 0; i < 2; i++) {
+        pg.pushMatrix();
+    
+        pg.translate(WIDTH/2 + STROKE_WIDTH, HEIGHT/2 + STROKE_WIDTH);
+        pg.rotate(i * PI);
+        
+        String letter = character(c);
+    
+        pg.text(letter, -WIDTH/2 + PADDING, -HEIGHT/2 + PADDING);
+        pg.text(suit_char, -WIDTH/2 + PADDING, -HEIGHT/2 + PADDING + 10);
+    
+        pg.popMatrix();
+      }  
+      
+      if (c == 0) {
+        pg.textFont(gbl);
+        pg.text(suit_char, WIDTH/2 + STROKE_WIDTH, HEIGHT/2 + STROKE_WIDTH - 3);
+      }
+  
+      pg.popMatrix();
+    }
+  }
+  
+  // Render back
+  pg.pushMatrix();
+  pg.translate(0*WIDTH_WITH_STROKE, 4*HEIGHT_WITH_STROKE);
+  
+  // Draw card
+  pg.fill(255);
+  pg.stroke(40);
+  pg.rect(0, 0, WIDTH, HEIGHT, 8);
+  
+  // Draw inlay
+  pg.fill(225, 95, 95);
+  pg.noStroke();
+  pg.rect(BACK_PADDING, BACK_PADDING, WIDTH-2*BACK_PADDING+1, HEIGHT-2*BACK_PADDING+1, 6);
+
+  pg.popMatrix();
+  
+  background(0);
+  image(pg, 0, 0);
+  
+  pg.save("../cards.png");
+  noLoop();
+}
--- src/render.zig
+++ src/render.zig
@@ -17,14 +17,10 @@ const CARD_BACK_WIDTH = @import("geom.zig").CARD_BACK_WIDTH;
 const CARD_BACK_HEIGHT = @import("geom.zig").CARD_BACK_HEIGHT;
 
 pub const Renderer = struct {
-    red_corner: ray.struct_Texture,
-    black_corner: ray.struct_Texture,
-    textures: std.AutoHashMap(Card, ray.RenderTexture2D),
-    texture_facedown: ray.RenderTexture2D,
+    cards: ray.Texture,
 
-    pub fn init(allocator: std.mem.Allocator, locations: *CardLocations) !Renderer {
-        const red_corner = loadPng("red.png");
-        const black_corner = loadPng("black.png");
+    pub fn init(locations: *CardLocations) !Renderer {
+        const cards = loadPng("cards.png");
 
         for (std.meta.tags(Card.Suit)) |suit| {
             for (std.meta.tags(Card.Rank)) |rank| {
@@ -32,51 +28,54 @@ pub const Renderer = struct {
             }
         }
 
-        var textures = std.AutoHashMap(Card, ray.RenderTexture2D).init(allocator);
-        for (std.meta.tags(Card.Suit)) |suit| {
-            for (std.meta.tags(Card.Rank)) |rank| {
-                const card = Card.of(rank, suit);
-                const texture = prerenderCard(card, red_corner, black_corner);
-
-                try textures.put(card, texture);
-            }
-        }
-
-        const texture_facedown = prerenderFacedownCard();
-
         return .{
-            .textures = textures,
-            .texture_facedown = texture_facedown,
-            .red_corner = red_corner,
-            .black_corner = black_corner,
+            .cards = cards,
         };
     }
 
     pub fn renderCard(renderer: *Renderer, card: Card, position: RotatedPosition, direction: Direction) void {
         const locus = position.locus;
 
-        const target: ray.RenderTexture2D = switch (direction) {
-            .faceup => renderer.textures.get(card) orelse unreachable,
-            .facedown => renderer.texture_facedown,
+        const target = renderer.cards;
+
+        const TexCoords = struct { x: f32, y: f32 };
+
+        const tex_coords: TexCoords = switch (direction) {
+            .faceup => block: {
+                const vertical_index: f32 = switch (card.suit) {
+                    .spades => 0.0,
+                    .hearts => 1.0,
+                    .diamonds => 2.0,
+                    .clubs => 3.0,
+                };
+
+                const x: f32 = @as(f32, @floatFromInt((card.rank.order() - 1))) * CARD_STROKE_WIDTH;
+                const y: f32 = vertical_index * CARD_STROKE_HEIGHT;
+
+                const coords: TexCoords = .{ .x = x, .y = y };
+
+                break :block coords;
+            },
+            .facedown => .{ .x = 0.0, .y = 4 * CARD_STROKE_HEIGHT },
         };
 
         ray.DrawTexturePro(
-            target.texture,
+            target,
             ray.Rectangle{
-                .x = 0,
-                .y = 0,
-                .width = @floatFromInt(target.texture.width),
-                .height = @floatFromInt(-target.texture.height),
+                .x = tex_coords.x,
+                .y = tex_coords.y,
+                .width = CARD_STROKE_WIDTH,
+                .height = CARD_STROKE_HEIGHT,
             },
             ray.Rectangle{
-                .x = locus.x - CARD_STROKE + @as(f32, @floatFromInt(target.texture.width)) / 2.0,
-                .y = locus.y - CARD_STROKE + @as(f32, @floatFromInt(target.texture.height)) / 2.0,
-                .width = @floatFromInt(target.texture.width),
-                .height = @floatFromInt(target.texture.height),
+                .x = locus.x - CARD_STROKE + CARD_STROKE_WIDTH / 2.0,
+                .y = locus.y - CARD_STROKE + CARD_STROKE_HEIGHT / 2.0,
+                .width = CARD_STROKE_WIDTH,
+                .height = CARD_STROKE_HEIGHT,
             },
             ray.Vector2{
-                .x = @as(f32, @floatFromInt(target.texture.width)) / 2.0,
-                .y = @as(f32, @floatFromInt(target.texture.height)) / 2.0,
+                .x = CARD_STROKE_WIDTH / 2.0,
+                .y = CARD_STROKE_HEIGHT / 2.0,
             },
             position.angle,
             ray.WHITE,
@@ -89,213 +88,6 @@ fn loadPng(comptime path: []const u8) ray.Texture {
     const image = ray.LoadImageFromMemory(".png", data, data.len);
 
     return ray.LoadTextureFromImage(image);
-}
-
-pub fn prerenderCard(card: Card, red_corner: ray.Texture, black_corner: ray.Texture) ray.RenderTexture2D {
-    const target: ray.RenderTexture2D = ray.LoadRenderTexture(CARD_STROKE_WIDTH, CARD_STROKE_HEIGHT);
-
-    const roundness = 0.25;
-    const segments = 20;
-
-    ray.BeginTextureMode(target);
-    defer ray.EndTextureMode();
-
-    ray.ClearBackground(ray.Color{ .a = 0.0, .r = 0.0, .b = 0.0, .g = 0.0 });
-
-    // Draw outline
-    {
-        const rect: ray.Rectangle = .{
-            .x = 0.0,
-            .y = 0.0,
-            .width = CARD_STROKE_WIDTH,
-            .height = CARD_STROKE_HEIGHT,
-        };
-        const outLineColor: ray.Color = .{ .a = 255, .r = 0, .g = 0, .b = 0 };
-
-        ray.DrawRectangleRounded(rect, roundness, segments, outLineColor);
-    }
-
-    // Draw body
-    {
-        const rect: ray.Rectangle = .{
-            .x = CARD_STROKE,
-            .y = CARD_STROKE,
-            .width = CARD_WIDTH,
-            .height = CARD_HEIGHT,
-        };
-        const bodyColor: ray.Color = .{ .a = 255, .r = 255, .g = 255, .b = 255 };
-
-        ray.DrawRectangleRounded(rect, roundness, segments, bodyColor);
-    }
-
-    prerenderDrawCornerRank(card, red_corner, black_corner);
-    prerenderDrawCornerSuit(card, red_corner, black_corner);
-
-    return target;
-}
-
-pub fn prerenderFacedownCard() ray.RenderTexture2D {
-    const target: ray.RenderTexture2D = ray.LoadRenderTexture(CARD_STROKE_WIDTH, CARD_STROKE_HEIGHT);
-
-    const roundness = 0.25;
-    const segments = 20;
-
-    ray.BeginTextureMode(target);
-    defer ray.EndTextureMode();
-
-    ray.ClearBackground(ray.Color{ .a = 0.0, .r = 0.0, .b = 0.0, .g = 0.0 });
-
-    {
-        const rect: ray.Rectangle = .{
-            .x = 0.0,
-            .y = 0.0,
-            .width = CARD_STROKE_WIDTH,
-            .height = CARD_STROKE_HEIGHT,
-        };
-        const outLineColor: ray.Color = .{ .a = 255, .r = 0, .g = 0, .b = 0 };
-
-        ray.DrawRectangleRounded(rect, roundness, segments, outLineColor);
-    }
-
-    // Draw body
-    {
-        const rect: ray.Rectangle = .{
-            .x = CARD_STROKE,
-            .y = CARD_STROKE,
-            .width = CARD_WIDTH,
-            .height = CARD_HEIGHT,
-        };
-        const bodyColor: ray.Color = .{ .a = 255, .r = 255, .g = 255, .b = 255 };
-
-        ray.DrawRectangleRounded(rect, roundness, segments, bodyColor);
-    }
-
-    {
-        const backRect: ray.Rectangle = .{
-            .x = CARD_STROKE + CARD_BACK_GUTTER,
-            .y = CARD_STROKE + CARD_BACK_GUTTER,
-            .width = CARD_BACK_WIDTH,
-            .height = CARD_BACK_HEIGHT,
-        };
-        const backColor: ray.Color = .{ .a = 255, .r = 225, .g = 95, .b = 95 };
-
-        ray.DrawRectangleRounded(backRect, 0.15, segments, backColor);
-    }
-
-    return target;
-}
-
-fn prerenderDrawCornerRank(card: Card, red_corner: ray.Texture, black_corner: ray.Texture) void {
-    const sprite_width = 12.0;
-
-    const tex = switch (card.suit) {
-        .hearts, .diamonds => red_corner,
-        .spades, .clubs => black_corner,
-    };
-
-    const suit_index: struct { x: f32, y: f32 } = switch (card.rank) {
-        .ace => .{ .x = 0, .y = 0 },
-        .two => .{ .x = 2, .y = 0 },
-        .three => .{ .x = 3, .y = 0 },
-        .four => .{ .x = 0, .y = 1 },
-        .five => .{ .x = 1, .y = 1 },
-        .six => .{ .x = 2, .y = 1 },
-        .seven => .{ .x = 3, .y = 1 },
-        .eight => .{ .x = 0, .y = 2 },
-        .nine => .{ .x = 1, .y = 2 },
-        .ten => .{ .x = 2, .y = 2 },
-        .jack => .{ .x = 3, .y = 2 },
-        .queen => .{ .x = 0, .y = 3 },
-        .king => .{ .x = 1, .y = 3 },
-    };
-
-    const src: ray.Rectangle = .{
-        .x = suit_index.x * sprite_width,
-        .y = suit_index.y * sprite_width,
-        .width = sprite_width,
-        .height = sprite_width,
-    };
-
-    const top_left: ray.Rectangle = .{
-        .x = CARD_STROKE,
-        .y = CARD_STROKE + 2.0,
-        .width = 12.0,
-        .height = 12.0,
-    };
-    const top_right: ray.Rectangle = .{
-        .x = CARD_STROKE + CARD_WIDTH - 13.0,
-        .y = CARD_STROKE + 2.0,
-        .width = 12.0,
-        .height = 12.0,
-    };
-    const bottom_left: ray.Rectangle = .{
-        .x = CARD_STROKE + 13.0,
-        .y = CARD_STROKE + CARD_HEIGHT - 2.0,
-        .width = 12.0,
-        .height = 12.0,
-    };
-    const bottom_right: ray.Rectangle = .{
-        .x = CARD_STROKE + CARD_WIDTH,
-        .y = CARD_STROKE + CARD_HEIGHT - 2.0,
-        .width = 12.0,
-        .height = 12.0,
-    };
-
-    ray.DrawTexturePro(tex, src, top_left, .{ .x = 0.0, .y = 0.0 }, 0.0, ray.WHITE);
-    ray.DrawTexturePro(tex, src, top_right, .{ .x = 0.0, .y = 0.0 }, 0.0, ray.WHITE);
-    ray.DrawTexturePro(tex, src, bottom_left, .{ .x = 0.0, .y = 0.0 }, 180.0, ray.WHITE);
-    ray.DrawTexturePro(tex, src, bottom_right, .{ .x = 0.0, .y = 0.0 }, 180.0, ray.WHITE);
-}
-
-fn prerenderDrawCornerSuit(card: Card, red_corner: ray.Texture, black_corner: ray.Texture) void {
-    const sprite_width = 12.0;
-
-    const tex = switch (card.suit) {
-        .hearts, .diamonds => red_corner,
-        .spades, .clubs => black_corner,
-    };
-
-    const suit_index: struct { x: f32, y: f32 } = switch (card.suit) {
-        .hearts, .clubs => .{ .x = 3, .y = 3 },
-        .diamonds, .spades => .{ .x = 2, .y = 3 },
-    };
-
-    const src: ray.Rectangle = .{
-        .x = suit_index.x * sprite_width,
-        .y = suit_index.y * sprite_width,
-        .width = sprite_width,
-        .height = sprite_width,
-    };
-
-    const top_left: ray.Rectangle = .{
-        .x = CARD_STROKE,
-        .y = CARD_STROKE + sprite_width + 2.0,
-        .width = sprite_width,
-        .height = sprite_width,
-    };
-    const top_right: ray.Rectangle = .{
-        .x = CARD_STROKE + CARD_WIDTH - sprite_width - 1.0,
-        .y = CARD_STROKE + sprite_width + 2.0,
-        .width = sprite_width,
-        .height = sprite_width,
-    };
-    const bottom_left: ray.Rectangle = .{
-        .x = CARD_STROKE + sprite_width + 1.0,
-        .y = CARD_STROKE + CARD_HEIGHT - sprite_width - 2.0,
-        .width = sprite_width,
-        .height = sprite_width,
-    };
-    const bottom_right: ray.Rectangle = .{
-        .x = CARD_STROKE + CARD_WIDTH,
-        .y = CARD_STROKE + CARD_HEIGHT - sprite_width - 2.0,
-        .width = sprite_width,
-        .height = sprite_width,
-    };
-
-    ray.DrawTexturePro(tex, src, top_left, .{ .x = 0.0, .y = 0.0 }, 0.0, ray.WHITE);
-    ray.DrawTexturePro(tex, src, top_right, .{ .x = 0.0, .y = 0.0 }, 0.0, ray.WHITE);
-    ray.DrawTexturePro(tex, src, bottom_left, .{ .x = 0.0, .y = 0.0 }, 180.0, ray.WHITE);
-    ray.DrawTexturePro(tex, src, bottom_right, .{ .x = 0.0, .y = 0.0 }, 180.0, ray.WHITE);
 }
 
 /// Render debug overlay
